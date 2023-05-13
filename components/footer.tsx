226a8693f003911cd62adaa35178c94617d50111
Anish Pallati <anishp@duck.com>
2023-05-13 16:47:34 -0400

    feat: Add hover cards to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,18 +1,68 @@
 import { Link } from "@/components/link";
 import { Content } from "@/components/content";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+	HoverCard,
+	HoverCardContent,
+	HoverCardTrigger,
+} from "@/components/ui/hover-card";
+
+interface UserCardProps {
+	name: string;
+	username: string;
+	description: string;
+}
+
+const UserCard = ({ name, username, description }: UserCardProps) => {
+	const url = `https://github.com/${username}`;
+	const avatarUrl = `${url}.png`;
+
+	const initials = name
+		.split(" ")
+		.map((n) => n[0].toUpperCase())
+		.join("");
+
+	return (
+		<HoverCard>
+			<HoverCardTrigger asChild>
+				<Link href={url} newWindow>
+					{name}
+				</Link>
+			</HoverCardTrigger>
+
+			<HoverCardContent className="w-50">
+				<div className="flex flex-row gap-x-4">
+					<Avatar>
+						<AvatarImage src={avatarUrl} />
+						<AvatarFallback>{initials}</AvatarFallback>
+					</Avatar>
+
+					<div className="space-y-1">
+						<h4 className="text-sm font-semibold">@{username}</h4>
+						<p className="text-sm">{description}</p>
+					</div>
+				</div>
+			</HoverCardContent>
+		</HoverCard>
+	);
+};
 
 export const Footer = () => {
 	return (
 		<Content as="footer" border="border-t" className="py-4">
 			<p className="text-muted-foreground">
 				Developed by{" "}
-				<Link href="https://github.com/ap-1" newWindow>
-					Anish Pallati
-				</Link>{" "}
+				<UserCard
+					name="Anish Pallati"
+					username="ap-1"
+					description="hi"
+				/>{" "}
 				and{" "}
-				<Link href="https://github.com/Kevin-Liu-01" newWindow>
-					Kevin Liu
-				</Link>{" "}
+				<UserCard
+					name="Kevin Liu"
+					username="Kevin-Liu-01"
+					description="🗿 what"
+				/>{" "}
 				for{" "}
 				<Link href="https://pantherhack-2023.devpost.com/" newWindow>
 					PantherHack 2023
