9716738d8eeb3bd6f141d18c968e12a212937952
Anish Pallati <anishp@duck.com>
2023-05-13 12:39:19 -0400

    feat: Initial bootstrap

---
+++ app/style.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import localFont from "next/font/local";
+
+import { cn } from "@/lib/utils";
+import { Provider } from "jotai";
+import { ThemeProvider } from "next-themes";
+
+import type { PropsWithChildren } from "react";
+
+import "./globals.css";
+
+const satoshi = localFont({
+	src: "../fonts/Satoshi-Variable.woff2",
+	variable: "--font-satoshi",
+});
+
+export const StyleProvider = ({ children }: PropsWithChildren) => {
+	return (
+		<ThemeProvider attribute="class" enableColorScheme={false}>
+			<style jsx global>
+				{`
+					:root {
+						--font-satoshi: ${satoshi.variable};
+					}
+				`}
+			</style>
+
+			<body className={cn(satoshi.variable, "font-sans")}>
+				<Provider>{children}</Provider>
+			</body>
+		</ThemeProvider>
+	);
+};
