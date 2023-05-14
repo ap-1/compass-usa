986ad3a00dda722fedb34ac5a0bf6ba02863f6da
Anish Pallati <anishp@duck.com>
2023-05-14 00:37:09 -0400

    revert: Remove LinkedIn API keys

--- env.mjs
+++ env.mjs
@@ -5,19 +5,14 @@ export const env = createEnv({
 	server: {
 		OPENAI_API_KEY: z.string().min(1),
 		CLERK_SECRET_KEY: z.string().min(1),
-		LINKEDIN_API_KEY: z.string().min(1),
 	},
 	client: {
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
-		NEXT_PUBLIC_LINKEDIN_CLIENT_ID: z.string().min(1),
 	},
 	runtimeEnv: {
 		OPENAI_API_KEY: process.env.OPENAI_API_KEY,
 		CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-		LINKEDIN_API_KEY: process.env.LINKEDIN_API_KEY,
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
 			process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-		NEXT_PUBLIC_LINKEDIN_CLIENT_ID:
-			process.env.NEXT_PUBLIC_LINKEDIN_CLIENT_ID,
 	},
 });
